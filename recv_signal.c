60e7ca94db8dd3c8b5a33c3b9aa9ca51235b459e
Jacob Tyree <tyreej@msoe.edu>
2024-11-14 18:01:40 -0600

    Modified send_signal.c

--- recv_signal.c
+++ recv_signal.c
@@ -4,3 +4,32 @@
  * 
  * Brief summary of program:
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+ void handle_sigusr1(int signum, siginfo_t *info, void *context);
+
+ int main() {
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handle_sigusr1;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        pause();
+    }
+    return 0;
+ }
+
+ void handle_sigusr1(int signum, siginfo_t *info, void *context) {
+    printf("Received SIGUSR1 with sival_int: %d\n", info->si_value.sival_int);
+ }
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,41 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Jacob Tyree
  * 
- * Brief summary of program:
+ * Brief summary of program: Sends a signal to another program for it to receive the SIGUSR1 signal.
+ * To send a signal, run the program that receives the signal, and then in another terminal, enter
+ * ps aux and find the PID for the program that is running. Then run this program with the command
+ * ./programname <PID>. 
  */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <target PID>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+
+    pid_t pid = atoi(argv[1]);
+    srand(time(NULL));
+    
+    int random_value = rand() % 100;
+
+    union sigval value; 
+    value.sival_int = random_value;
+
+    if(sigqueue(pid, SIGUSR1, value) == -1) {
+        perror("sigerror");
+        exit(EXIT_FAILURE);
+    }
+    printf("Sent SIGUSR1 with random integer %d to process %d\n", random_value, pid);
+    
+    return 0;
+}
